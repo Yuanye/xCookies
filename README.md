5b5d3d71344c25556792edddc071768db4bb9965
Yuanye <geyuanye@gmail.com>
2015-12-28 16:26:14 +0800

    Add first version

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # xCookies
 Chrome extension for Alimama cookies
+
+### 使用规则
+
+* 点击扩展插件以后，等待弹窗信息即可。
+
+### TODO
+
+* 待后端写完去重，改为开启浏览器后自动发送。
---
+++ index.js
@@ -0,0 +1,54 @@
+//  geyuanye@gmail.com
+
+if (!chrome.cookies) {
+  chrome.cookies = chrome.experimental.cookies;
+}
+
+function Timer() {
+  this.start_ = new Date();
+
+  this.elapsed = function() {
+    return (new Date()) - this.start_;
+  }
+
+  this.reset = function() {
+    this.start_ = new Date();
+  }
+}
+
+function sendCookies(data) {
+  $.ajax({
+  type: "POST",
+  url: "https://bi.liwushuo.com/api/ali/cookies",
+  contentType: 'application/json',
+  data: JSON.stringify({"data": data}),
+  dataType: "json",
+  success: function () {
+    alert("Success! Thanks!");
+  },
+  error: function(response) {
+    alert("Failed!")
+  }
+});
+}
+
+function onload() {
+  var timer = new Timer();
+  chrome.cookies.getAll({}, function(cookies) {
+    start = new Date();
+    for (var i in cookies) {
+      var cookie = cookies[i]
+      if (cookie.domain === '.alimama.com' & cookie.name === 'cookie2') {
+        console.log(cookie.domain + " : " + cookie.name + " : " + cookie.value)
+        var data = {};
+        data["cookies"] = cookie.name + ":" + cookie.value;
+        sendCookies(data)
+      }
+    }
+    timer.reset();
+  });
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  onload();
+});
